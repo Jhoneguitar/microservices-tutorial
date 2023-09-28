0f61225c01c807b861b8a8d397132e14b767a011
Jhon Edison Escobar Londoño <jhonacustica@hotmail.com>
2023-09-28 17:02:41 -0500

    Se agrega el proyecto

---
+++ car-service/src/test/java/com/tutorialmicroservicios/carservice/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialmicroservicios.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/main/java/com/tutorialmicroservicios/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tutorialmicroservicios.userservice.repository;
+
+import com.tutorialmicroservicios.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
